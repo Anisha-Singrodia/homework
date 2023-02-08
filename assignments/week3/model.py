333357764c281ceaf3bbd266d281816a9fe4fd0a
Anisha Singrodia <anishasingrodia@Anishas-MacBook-Air.local>
2023-02-08 00:18:59 -0500

    removed dropout

--- assignments/week3/model.py
+++ assignments/week3/model.py
@@ -46,10 +46,10 @@ class MLP(torch.nn.Module):
         # x = self.initializer(x)
         # x = x.view(-1, 28 * 28)
         x = F.relu(self.fc1(x))
-        x = self.droput(x)
+        # x = self.droput(x)
         for i in range(self.hidden_count):
             x = F.relu(self.fc2(x))
-        x = self.droput(x)
+        # x = self.droput(x)
         x = self.fc3(x)
         output = F.log_softmax(x, dim=1)
         return output
