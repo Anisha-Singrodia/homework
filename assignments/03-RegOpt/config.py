de2fef9a5e9fdc22671f05b8d4dd7fca601f1b39
Anisha Singrodia <anishasingrodia@Anishas-MacBook-Air.local>
2023-02-19 01:32:49 -0500

    new lr implemented

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -6,15 +6,17 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 
 
 class CONFIG:
-    batch_size = 64
-    num_epochs = 2
+    batch_size = 32
+    num_epochs = 5
     initial_learning_rate = 0.001
     initial_weight_decay = 0
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
-        "T_max": batch_size + num_epochs,
+        # "T_max": batch_size + num_epochs,
+        "T_0": 200,
+        "T_mult": 1,
         "eta_min": 0,
         "last_epoch": -1,
         "verbose": False,
