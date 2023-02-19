a591f30ab6555988ef91b4370783a6a017b79e45
Anisha Singrodia <anishasingrodia@Anishas-MacBook-Air.local>
2023-02-19 13:12:38 -0500

    reduced epoch

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -6,8 +6,8 @@ from torchvision.transforms import Compose, Normalize, ToTensor, RandomGrayscale
 
 
 class CONFIG:
-    batch_size = 64
-    num_epochs = 5
+    batch_size = 128
+    num_epochs = 2
     initial_learning_rate = 0.001
     initial_weight_decay = 0
 
