af4499a596bc985b228a4f05b27bf9c08117f1bf
Anisha Singrodia <anishasingrodia@Anishas-MacBook-Air.local>
2023-02-19 16:14:58 -0500

    tuning

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -2,21 +2,23 @@ from typing import Callable
 import torch
 import torch.optim
 import torch.nn as nn
-from torchvision.transforms import Compose, Normalize, ToTensor, RandomGrayscale
+from torchvision.transforms import Compose, Normalize, ToTensor
+
+# Resize, RandomRotation, RandomHorizontalFlip, ColorJitter, RandomAdjustSharpness, RandomErasing
 
 
 class CONFIG:
-    batch_size = 135
-    num_epochs = 2
-    initial_learning_rate = 0.002
+    batch_size = 64
+    num_epochs = 5
+    initial_learning_rate = 0.003
     initial_weight_decay = 0
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
-        "T_max": batch_size + num_epochs,
-        # "T_0": 200,
-        # "T_mult": 1,
+        # "T_max": (batch_size + num_epochs),
+        "T_0": 1,
+        "T_mult": 2,
         "eta_min": 0,
         "last_epoch": -1,
         "verbose": False,
@@ -34,6 +36,13 @@ class CONFIG:
         [
             ToTensor(),
             Normalize((0.4915, 0.4823, 0.4468), (0.2470, 0.2435, 0.2616)),
-            RandomGrayscale(),
+            # Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            # Resize((32, 32)), # Resize the image in a 32X32 shape
+            # RandomRotation(10), # Randomly rotate some images by 20 degrees
+            # RandomHorizontalFlip(0.1), # Randomly horizontal flip the images
+            # ColorJitter(brightness = 0.1, contrast = 0.1,
+            #                     saturation = 0.1), #reduces
+            # RandomAdjustSharpness(sharpness_factor = 2, p = 0.1),
+            # RandomErasing(p=0.75,scale=(0.02, 0.1),value=1.0, inplace=False)
         ]
     )
--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -12,7 +12,7 @@ class CustomLRScheduler(_LRScheduler):
         _LRScheduler (_type_): Custom LR Scheduler
     """
 
-    def __init__(self, optimizer, T_max, eta_min=0, last_epoch=-1, verbose=False):
+    def __init__(self, optimizer, T_0, T_mult, eta_min=0, last_epoch=-1, verbose=False):
         """
         Create a new scheduler.
 
@@ -21,17 +21,17 @@ class CustomLRScheduler(_LRScheduler):
 
         """
         # ... Your Code Here ...
-        self.T_max = T_max
-        self.eta_min = eta_min
-        # if T_0 <= 0 or not isinstance(T_0, int):
-        #     raise ValueError("Expected positive integer T_0, but got {}".format(T_0))
-        # if T_mult < 1 or not isinstance(T_mult, int):
-        #     raise ValueError("Expected integer T_mult >= 1, but got {}".format(T_mult))
-        # self.T_0 = T_0
-        # self.T_i = T_0
-        # self.T_mult = T_mult
+        # self.T_max = T_max
         # self.eta_min = eta_min
-        # self.T_cur = last_epoch
+        if T_0 <= 0 or not isinstance(T_0, int):
+            raise ValueError("Expected positive integer T_0, but got {}".format(T_0))
+        if T_mult < 1 or not isinstance(T_mult, int):
+            raise ValueError("Expected integer T_mult >= 1, but got {}".format(T_mult))
+        self.T_0 = T_0
+        self.T_i = T_0
+        self.T_mult = T_mult
+        self.eta_min = eta_min
+        self.T_cur = last_epoch
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
 
     def get_lr(self) -> List[float]:
@@ -40,65 +40,120 @@ class CustomLRScheduler(_LRScheduler):
         Returns:
             List[float]: list of all the schedulers
         """
-        # if not self._get_lr_called_within_step:
-        #     warnings.warn(
-        #         "To get the last learning rate computed by the scheduler, "
-        #         "please use `get_last_lr()`.",
-        #         UserWarning,
-        #     )
+        if not self._get_lr_called_within_step:
+            warnings.warn(
+                "To get the last learning rate computed by the scheduler, "
+                "please use `get_last_lr()`.",
+                UserWarning,
+            )
 
-        # return [
-        #     self.eta_min
-        #     + (base_lr - self.eta_min)
-        #     * (1 + math.cos(math.pi * self.T_cur / self.T_i))
-        #     / 2
-        #     for base_lr in self.base_lrs
-        # ]
+        return [
+            self.eta_min
+            + (base_lr - self.eta_min)
+            * (1 + math.cos(math.pi * self.T_cur / self.T_i))
+            / 2
+            for base_lr in self.base_lrs
+        ]
+
+    def step(self, epoch=None):
+        """Step could be called after every batch update"""
+
+        if epoch is None and self.last_epoch < 0:
+            epoch = 0
+
+        if epoch is None:
+            epoch = self.last_epoch + 1
+            self.T_cur = self.T_cur + 1
+            if self.T_cur >= self.T_i:
+                self.T_cur = self.T_cur - self.T_i
+                self.T_i = self.T_i * self.T_mult
+        else:
+            if epoch < 0:
+                raise ValueError(
+                    "Expected non-negative epoch, but got {}".format(epoch)
+                )
+            if epoch >= self.T_0:
+                if self.T_mult == 1:
+                    self.T_cur = epoch % self.T_0
+                else:
+                    n = int(
+                        math.log(
+                            (epoch / self.T_0 * (self.T_mult - 1) + 1), self.T_mult
+                        )
+                    )
+                    self.T_cur = epoch - self.T_0 * (self.T_mult**n - 1) / (
+                        self.T_mult - 1
+                    )
+                    self.T_i = self.T_0 * self.T_mult ** (n)
+            else:
+                self.T_i = self.T_0
+                self.T_cur = epoch
+        self.last_epoch = math.floor(epoch)
+
+        class _enable_get_lr_call:
+            def __init__(self, o):
+                self.o = o
+
+            def __enter__(self):
+                self.o._get_lr_called_within_step = True
+                return self
+
+            def __exit__(self, type, value, traceback):
+                self.o._get_lr_called_within_step = False
+                return self
+
+        with _enable_get_lr_call(self):
+            for i, data in enumerate(zip(self.optimizer.param_groups, self.get_lr())):
+                param_group, lr = data
+                param_group["lr"] = lr
+                self.print_lr(self.verbose, i, lr, epoch)
+
+        self._last_lr = [group["lr"] for group in self.optimizer.param_groups]
         # Note to students: You CANNOT change the arguments or return type of
         # this function (because it is called internally by Torch)
 
         # ... Your Code Here ...
         # Here's our dumb baseline implementation:
         # return [i for i in self.base_lrs]
-        if not self._get_lr_called_within_step:
-            warnings.warn(
-                "To get the last learning rate computed by the scheduler, please use `get_last_lr()`.",
-                UserWarning,
-            )
+        # if not self._get_lr_called_within_step:
+        #     warnings.warn(
+        #         "To get the last learning rate computed by the scheduler, please use `get_last_lr()`.",
+        #         UserWarning,
+        #     )
 
-        if self.last_epoch == 0:
-            # print("in 1")
-            return [group["lr"] for group in self.optimizer.param_groups]
-        elif self._step_count == 1 and self.last_epoch > 0:
-            # print("in 2")
-            return [
-                self.eta_min
-                + (base_lr - self.eta_min)
-                * (1 + math.cos((self.last_epoch) * math.pi / self.T_max))
-                / 2
-                for base_lr, group in zip(self.base_lrs, self.optimizer.param_groups)
-            ]
-        elif (self.last_epoch - 1 - self.T_max) % (2 * self.T_max) == 0:
-            # print("in 3")
-            return [
-                group["lr"]
-                + (base_lr - self.eta_min) * (1 - math.cos(math.pi / self.T_max)) / 2
-                for base_lr, group in zip(self.base_lrs, self.optimizer.param_groups)
-            ]
-        # print("in 4")
-        return [
-            (1 + math.cos(math.pi * self.last_epoch / self.T_max))
-            / (1 + math.cos(math.pi * (self.last_epoch - 1) / self.T_max))
-            * (group["lr"] - self.eta_min)
-            + self.eta_min
-            for group in self.optimizer.param_groups
-        ]
+        # if self.last_epoch == 0:
+        #     # print("in 1")
+        #     return [group["lr"] for group in self.optimizer.param_groups]
+        # elif self._step_count == 1 and self.last_epoch > 0:
+        #     # print("in 2")
+        #     return [
+        #         self.eta_min
+        #         + (base_lr - self.eta_min)
+        #         * (1 + math.cos((self.last_epoch) * math.pi / self.T_max))
+        #         / 2
+        #         for base_lr, group in zip(self.base_lrs, self.optimizer.param_groups)
+        #     ]
+        # elif (self.last_epoch - 1 - self.T_max) % (2 * self.T_max) == 0:
+        #     # print("in 3")
+        #     return [
+        #         group["lr"]
+        #         + (base_lr - self.eta_min) * (1 - math.cos(math.pi / self.T_max)) / 2
+        #         for base_lr, group in zip(self.base_lrs, self.optimizer.param_groups)
+        #     ]
+        # # print("in 4")
+        # return [
+        #     (1 + math.cos(math.pi * self.last_epoch / self.T_max))
+        #     / (1 + math.cos(math.pi * (self.last_epoch - 1) / self.T_max))
+        #     * (group["lr"] - self.eta_min)
+        #     + self.eta_min
+        #     for group in self.optimizer.param_groups
+        # ]
 
-    def _get_closed_form_lr(self):
-        return [
-            self.eta_min
-            + (base_lr - self.eta_min)
-            * (1 + math.cos(math.pi * self.last_epoch / self.T_max))
-            / 2
-            for base_lr in self.base_lrs
-        ]
+    # def _get_closed_form_lr(self):
+    #     return [
+    #         self.eta_min
+    #         + (base_lr - self.eta_min)
+    #         * (1 + math.cos(math.pi * self.last_epoch / self.T_max))
+    #         / 2
+    #         for base_lr in self.base_lrs
+    #     ]
