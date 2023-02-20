bbb82537350beaa5e57b94939133054f80840b31
Anisha Singrodia <anishasingrodia@ist-00166.apn.wlan.private.upenn.edu>
2023-02-20 11:26:13 -0500

    tuning

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -11,7 +11,7 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 
 class CONFIG:
     batch_size = 128
-    num_epochs = 10
+    num_epochs = 5
     initial_learning_rate = 0.005
     initial_weight_decay = 1e-5
 
