994f984cf283214cba3ff29ae7e9f7234e451194
Anisha Singrodia <anishasingrodia@Anishas-MacBook-Air.local>
2023-02-21 01:55:12 -0500

    tuning

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -11,14 +11,14 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 
 class CONFIG:
     batch_size = 128
-    num_epochs = 1
+    num_epochs = 10
     initial_learning_rate = 0.005
     initial_weight_decay = 0
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
-        "T_max": (batch_size * 4),
+        "T_max": (batch_size * 10),
         # "T_0": 8,
         # "T_mult": 1,
         "eta_min": 1e-4,
