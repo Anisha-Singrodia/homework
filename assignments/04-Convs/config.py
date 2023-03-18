28ec67e6db978cb4680f0fe1282ab7cc4c6c896a
singroa <singrodia.anisha@gmail.com>
2023-03-18 19:21:38 -0400

    tune

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -2,11 +2,11 @@ from typing import Callable
 import torch
 import torch.optim
 import torch.nn as nn
-from torchvision.transforms import Compose, ToTensor
+from torchvision.transforms import Compose, ToTensor, Normalize
 
 
 class CONFIG:
-    batch_size = 256
+    batch_size = 180
     num_epochs = 1
 
     optimizer_factory: Callable[
