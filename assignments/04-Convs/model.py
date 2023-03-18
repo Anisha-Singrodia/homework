ac504d7b6c6a0794c9bc3b94516fe1a5134e2033
singroa <singrodia.anisha@gmail.com>
2023-03-18 17:41:59 -0400

    tuning

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -31,30 +31,34 @@ class Model(nn.Module):
     """
 
     def __init__(self, num_channels: int, num_classes: int) -> None:
+
         super().__init__()
-
-        self.conv = nn.Sequential(
-            ConvBlock(num_channels, out_channels=32),
-            # ConvBlock(in_channels=64, out_channels=128),
-            # ConvBlock(in_channels=128, out_channels=256),
-            # ConvBlock(in_channels=256, out_channels=512),
-        )
-
-        self.fc = nn.Sequential(
-            # nn.Dropout(0.25),
-            # nn.Linear(64, 64),
-            nn.ReLU(),
-            nn.BatchNorm1d(32),
-            nn.Dropout(0.2),
-            nn.Linear(32, num_classes),
+        self.model = torch.nn.Sequential(
+            # Input = 3 x 32 x 32, Output = 32 x 32 x 32
+            torch.nn.Conv2d(
+                in_channels=num_channels, out_channels=32, kernel_size=3, padding=1
+            ),
+            torch.nn.ReLU(),
+            # Input = 32 x 32 x 32, Output = 32 x 16 x 16
+            torch.nn.MaxPool2d(kernel_size=2),
+            # Input = 32 x 16 x 16, Output = 64 x 16 x 16
+            torch.nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+            torch.nn.ReLU(),
+            # Input = 64 x 16 x 16, Output = 64 x 8 x 8
+            torch.nn.MaxPool2d(kernel_size=2),
+            # #Input = 64 x 8 x 8, Output = 64 x 8 x 8
+            # torch.nn.Conv2d(in_channels = 64, out_channels = 64, kernel_size = 3, padding = 1),
+            # torch.nn.ReLU(),
+            # #Input = 64 x 8 x 8, Output = 64 x 4 x 4
+            # torch.nn.MaxPool2d(kernel_size=2),
+            torch.nn.Flatten(),
+            torch.nn.Linear(64 * 8 * 8, 512),
+            torch.nn.ReLU(),
+            torch.nn.Linear(512, num_classes),
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
         forward method
         """
-        x = self.conv(x)
-        x = torch.mean(x, dim=3)
-        x, _ = torch.max(x, dim=2)
-        x = self.fc(x)
-        return x
+        return self.model(x)
