80c0724da2ada50a482ae1b0d9a9a975cd4e27b0
singroa <singrodia.anisha@gmail.com>
2023-03-18 18:14:47 -0400

    tune

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -2,7 +2,7 @@ from typing import Callable
 import torch
 import torch.optim
 import torch.nn as nn
-from torchvision.transforms import Compose, ToTensor
+from torchvision.transforms import Compose, ToTensor, Normalize
 
 
 class CONFIG:
@@ -13,4 +13,11 @@ class CONFIG:
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(model.parameters(), lr=0.005)
 
-    transforms = Compose([ToTensor()])
+    transforms = Compose(
+        [
+            ToTensor(),
+            # Normalize((0.4915, 0.4823, 0.4468), (0.2470, 0.2435, 0.2616)),
+            Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            # Normalize(mean=[0.485, 0.456, 0.4], std=[0.229, 0.224, 0.2])
+        ]
+    )
