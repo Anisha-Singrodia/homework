6c6e0931e3a29e7353716a678678f24cb122b1a6
singroa <singrodia.anisha@gmail.com>
2023-03-18 21:43:03 -0400

    tune

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -2,22 +2,22 @@ from typing import Callable
 import torch
 import torch.optim
 import torch.nn as nn
-from torchvision.transforms import Compose, ToTensor, Normalize
+from torchvision.transforms import Compose, ToTensor
 
 
 class CONFIG:
-    batch_size = 180
+    batch_size = 256
     num_epochs = 1
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
-    ] = lambda model: torch.optim.Adam(model.parameters(), lr=0.006, weight_decay=0.003)
+    ] = lambda model: torch.optim.Adam(model.parameters(), lr=0.01)
 
     transforms = Compose(
         [
             ToTensor(),
             # Normalize((0.4915, 0.4823, 0.4468), (0.2470, 0.2435, 0.2616)),
-            Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            # Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
             # Normalize(mean=[0.485, 0.456, 0.4], std=[0.229, 0.224, 0.2])
         ]
     )
--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -11,40 +11,35 @@ class Model(nn.Module):
         super().__init__()
         # Input = 3 x 32 x 32, Output = 32 x 30 x 30
         self.conv_layer1 = nn.Conv2d(
-            in_channels=num_channels, out_channels=32, kernel_size=3
+            in_channels=num_channels, out_channels=16, kernel_size=3, stride=2
         )
         # Input = 32 x 30 x 30, Output = 32 x 28 x 28
-        self.conv_layer2 = nn.Conv2d(in_channels=32, out_channels=16, kernel_size=3)
+        self.conv_layer2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3)
         # Input = 32 x 28 x 28, Output = 32 x 14 x 14 =
         self.max_pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-
         # Input = 32 x 14 x 14, Output = 64 x 12 x 12
         self.conv_layer3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3)
         # Input = 64 x 12 x 12, Output = 64 x 10 x 10
         self.conv_layer4 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3)
         # Input = 64 x 10 x 10, Output = 64 x 5 x 5 = 1600
         self.max_pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        # self.fc1 = nn.Linear(6272, 128)
-        # self.relu1 = nn.ReLU()
-        self.fc2 = nn.Linear(3136, num_classes)
+        self.fc1 = nn.Linear(784, 128)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Linear(128, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
         forward method
         """
         out = self.conv_layer1(x)
-        out = self.conv_layer2(out)
+        # out = self.relu1(out)
+        # out = self.conv_layer2(out)
         out = self.max_pool1(out)
-
         # out = self.conv_layer3(out)
         # out = self.conv_layer4(out)
         # out = self.max_pool2(out)
-
         out = out.reshape(out.size(0), -1)
-
-        # out = self.fc1(out)
-        # out = self.relu1(out)
-        out = nn.Dropout(0.5)(out)
+        out = self.fc1(out)
+        out = self.relu1(out)
         out = self.fc2(out)
         return out
